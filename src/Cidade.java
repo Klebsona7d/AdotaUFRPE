e03d38289e3bf1368c38f4070941a89efea866f2
InspirAda <info.inspirada@gmail.com>
2016-10-31 10:50:55 -0300

    Teste

--- src/Cidade.java
+++
@@ -1,8 +0,0 @@
-class Cidade {
-	private String nome;
-	private String uf;
-	
-	public Cidade() {
-		System.out.println("Objeto da classe Cidade instanciado com sucesso!");
-	}
-}
--- src/PessoaJuridica.java
+++
@@ -1,9 +0,0 @@
-class PessoaJuridica {
-	private String razaoSocial;
-	private String nomeFantasia;
-	private String cnpj;
-	
-	public PessoaJuridica() {
-		System.out.println("Objeto da classe PessoaJuridica instanciado com sucesso!");
-	}
-}
